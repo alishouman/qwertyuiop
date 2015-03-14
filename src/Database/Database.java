1a01c2b18c544606f56eb2665f33907c5c8bbe90
alishouman <alishouman@cmail.carleton.ca>
2015-03-14 19:35:48 -0400

    Login function

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Database {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/3303Project";
+   static final String DB_URL = "jdbc:mysql://localhost/3303server";
 
    //  Database credentials
    static final String USER = "root";
