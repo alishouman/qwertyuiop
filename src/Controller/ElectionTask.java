6fc3d42b7900046ed10c2148246a6390d00b5d61
alishouman <alishouman@cmail.carleton.ca>
2015-04-08 11:01:27 -0400

    asd

--- src/Controller/ElectionTask.java
+++ src/Controller/ElectionTask.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,18 +12,22 @@ import View.ElectionResult;
 
 public class ElectionTask extends TimerTask {
 int serverNumber;
-JFrame frame;
+ElectionResult result ;
+JFrame frame=new JFrame();
 	public ElectionTask(int number){
 	serverNumber=number;
 	frame=new JFrame();
+	 result = new ElectionResult(null,null);
 	Timer time = new Timer(); // Instantiate Timer Object
 	
 	time.schedule(this, 0, 15000);
 }
 	@Override
 	public void run() {
-		
-   	 
+		//frame.dispose();
+	frame.remove(result);
+		frame.setSize(1000,1000);
+		frame.setLayout(new BorderLayout());
     	Database database = new Database(serverNumber);
 		ArrayList<String> candidates = database.getCandidates(serverNumber);
 		int[] values = new int[candidates.size()];
@@ -32,12 +37,13 @@ JFrame frame;
 		String[] names = new String[candidates.size()];
 		for (int i = 0; i < candidates.size(); i++)
 			names[i] = candidates.get(i);
-		ElectionResult result = new ElectionResult(values, names);
-		frame.setSize(1000, 1000);
+	 result = new ElectionResult(values, names);
+		
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-		frame.add(result);
+		//frame.removeAll();
+		frame.add(result,BorderLayout.CENTER);
+	
 		frame.revalidate();
 	}
 
